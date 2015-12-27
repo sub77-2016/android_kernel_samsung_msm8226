d090321a1d55f28327ee2cbd31499f8698cdbc60
Apavayan Sinha <info@apavayan.com>
2015-12-27 10:42:15 +0530

    security: Merge from the kmini3g
    Conflicts:
    	security/sdp/cache_cleanup.c
    	security/sdp/dek.c
    	security/sdp/dek_aes.c
    	security/sdp/dek_sysfs.c
    	security/sdp/fs_handler.c
    	security/sdp/kek_pack.c
    	security/sdp/pub_crypto_emul.c
    	security/sdp/sdp_mm.c

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2244,7 +2244,7 @@ out:
 }
 
 /**
- * security_genfs_sid - Obtain a SID for a file in a filesystem
+ * __security_genfs_sid - Helper to obtain a SID for a file in a filesystem
  * @fstype: filesystem type
  * @path: path from root of mount
  * @sclass: file security class
@@ -2253,11 +2253,13 @@ out:
  * Obtain a SID to use for a file in a filesystem that
  * cannot support xattr or use a fixed labeling behavior like
  * transition SIDs or task SIDs.
+ *
+ * The caller must acquire the policy_rwlock before calling this function.
  */
-int security_genfs_sid(const char *fstype,
-		       char *path,
-		       u16 orig_sclass,
-		       u32 *sid)
+static inline int __security_genfs_sid(const char *fstype,
+				       char *path,
+				       u16 orig_sclass,
+				       u32 *sid)
 {
 	int len;
 	u16 sclass;
@@ -2267,8 +2269,6 @@ int security_genfs_sid(const char *fstype,
 
 	while (path[0] == '/' && path[1] == '/')
 		path++;
-
-	read_lock(&policy_rwlock);
 
 	sclass = unmap_class(orig_sclass);
 	*sid = SECINITSID_UNLABELED;
@@ -2303,8 +2303,30 @@ int security_genfs_sid(const char *fstype,
 	*sid = c->sid[0];
 	rc = 0;
 out:
-	read_unlock(&policy_rwlock);
 	return rc;
+}
+
+/**
+ * security_genfs_sid - Obtain a SID for a file in a filesystem
+ * @fstype: filesystem type
+ * @path: path from root of mount
+ * @sclass: file security class
+ * @sid: SID for path
+ *
+ * Acquire policy_rwlock before calling __security_genfs_sid() and release
+ * it afterward.
+ */
+int security_genfs_sid(const char *fstype,
+		       char *path,
+		       u16 orig_sclass,
+		       u32 *sid)
+{
+	int retval;
+
+	read_lock(&policy_rwlock);
+	retval = __security_genfs_sid(fstype, path, orig_sclass, sid);
+	read_unlock(&policy_rwlock);
+	return retval;
 }
 
 /**
@@ -2320,6 +2342,7 @@ int security_fs_use(
 {
 	int rc = 0;
 	struct ocontext *c;
+	u32 tmpsid;
 
 	read_lock(&policy_rwlock);
 
@@ -2336,7 +2359,8 @@ int security_fs_use(
 		printk(KERN_INFO "SELinux: fstype:%s, behavior:%d, con_sid:%d \n", c->u.name, c->v.behavior, c->sid[0]);
 		if (!c->sid[0] || c->sid[0] == SECINITSID_UNLABELED ) {
 			rc = sidtab_context_to_sid(&sidtab, &c->context[0],
-						   &c->sid[0]);
+						   &tmpsid);
+			c->sid[0] = tmpsid;
 			if (rc)
 				goto out;
 		}
@@ -2345,7 +2369,8 @@ int security_fs_use(
 		printk(KERN_INFO "SELinux: fstype:%s assigned superblock SID %d \n", c->u.name, *sid);
 
 	} else {
-		rc = security_genfs_sid(fstype, "/", SECCLASS_DIR, sid);
+		rc = __security_genfs_sid(fstype, "/", SECCLASS_DIR,
+					sid);
 		if (rc) {
 			*behavior = SECURITY_FS_USE_NONE;
 			rc = 0;
