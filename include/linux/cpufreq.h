2534c299ddfeeb6f41c660e4fb2b41673f09db6b
Antonio <antonio.tagliapietra@hotmail.com>
2016-03-28 13:14:55 +0200

    cpufreq.h : add zzmove governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -466,6 +466,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 
